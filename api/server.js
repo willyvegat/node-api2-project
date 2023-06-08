bfce300479911d0d7e5bec43d14f34f85477fa2e
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-06-08 16:59:17 -0400

    created postsRouter and connected to posts-router.js

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@
 // require your posts router and connect it here
 
 const express = require('express');
+const postsRouter = require('./posts/posts-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', postsRouter);
 
 server.use('*', (req, res) => {
     res.status(404).json({
