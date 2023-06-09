0131d8dda6e62fa794b3ffd3562979f93712659a
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-06-08 22:06:07 -0400

    get request of posts created

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,38 @@
-// implement your posts router here
 const express = require('express');
-
+const Post = require('./posts-model');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    Post.find()
+        .then(found => {
+            res.json(found);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+                err: err.message
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.get('/:id/messages', (req, res) => {
+
+})
 
 module.exports = router;
